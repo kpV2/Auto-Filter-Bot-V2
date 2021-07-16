89daefb9ae57362336e125d2bef5e00d6ca4ef77
kpV2 <86794853+kpV2@users.noreply.github.com>
2021-07-16 09:22:14 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,15 +1,12 @@
 class script(object):
 
 
-    START_MSG = """ <b>Hi {}
+    START_MSG = """ <b>வணக்கம் {}
+உங்களுக்கு தேவைப்படும் புத்தகங்களை 
+முதலில் நமது சேனலில் 🔎 தேடிப் பாருங்கள். 
+ஏனெனில் உங்களுக்கு தேவையான பெரும்பாலான நூல்கள் ஏற்கனவே நமது சேனலில் பதிவிடப்பட்டுள்ளது. 
+நமது சேனலில் தேடியும் கிடைக்கவில்லையென்றால் மட்டும் நமது குழுவில் கேட்கவும்.
 
-You can call this as an Auto Filter Bot if you like :D
-
-This is Version 2 of Auto Filter Bot
-
-Bot gives button link to files in connected channels on query !
-
-No need to add filters for your files or movies from now on!
 
 For more click <i>help</i></b>"""
 
@@ -17,53 +14,22 @@ For more click <i>help</i></b>"""
     HELP_MSG = """<b>How to use the bot??</b>
 
 <i>
-* Add bot to your group with admin rights.
+* 
 
-* Add bot to channels which you want to link with <b>all admin rights</b>!
-</i>
-
-
-<b>Bot Commands - Works in Group only</b>
-(You need to be a Auth User in order to use these commands)
-
-* <code>/add channelid</code>  -  Links channel to your group.
-or
-* <code>/add @channelusername</code> - Links channel to your group.
-
-<i>NOTE : You can get your channel ID from @ChannelidHEXbot </i>
-
-
-* <code>/del channelid</code>  -  Delinks channel from group
-or
-* <code>/del @channelusername</code>  -  Delinks channel from group
-
-<i>NOTE : You can get connected channel details by <code>/filterstats</code> </i>
-
-
-* <code>/delall</code>  -  Removes all connected channels and filters from group!
-
-<i>Note : Dont add command delete bots in group! Otherwise, delall command wont work</i>
-
-
-* <code>/filterstats</code>  -  Check connected channels and number of filters.
-
+* 
 
 
 No need add each filter again!
 Bot will automatically search for your files and give links to that!
 
 
-<b>© @TroJanzHEX</b>"""
+<b>© @Educationworldd """
 
 
     ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
     
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>group 1 :</b> @Educationworldd
 
-⭕️<b>Language :</b> <code>Python3</code>
 
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
-
-⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
 
 """
